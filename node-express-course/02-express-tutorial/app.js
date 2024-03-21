eadbcf654b47553d9448bce11a04793ff112ff59
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-21 18:22:42 +0530

    express running

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,8 +1,19 @@
-const http = require("http");
+const express = require('express')
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end('home page')
-});
+const app = express()
 
-server.listen(4000);
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+})
